1f7fe12a5760bb413ff599edb51f3229421dcc6d
matheusAMDS <matheusa56@gmail.com>
2023-12-31 23:28:13 +0000

    feat: 'icon' prop form input

--- @/components/ui/input.tsx
+++ @/components/ui/input.tsx
@@ -3,17 +3,33 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactElement
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
+  ({ className, type, icon, ...props }, ref) => {
+    const classes = cn(
+      "flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2",
+      className
+    )
+
+    return icon ? (
+      <span className={classes}>
+        <span>
+          {icon}
+        </span>
+        <input
+          type={type}
+          className='w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50'
+          ref={ref}
+          {...props}
+        />
+      </span>
+    ) : (
       <input
         type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
+        className={classes}
         ref={ref}
         {...props}
       />
